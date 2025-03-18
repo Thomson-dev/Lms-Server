3e5188af1ea501026d907c8da1a4f09713eeee43
Thomson-dev <tomsinonyedikachi@gmail.com>
2025-03-18 23:31:36 +0100

    new changes

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { newPayment } from "../controllers/OrderController.js";
+import { newPayment } from "../controllers/courseOrder.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const orderRouter = express.Router();
